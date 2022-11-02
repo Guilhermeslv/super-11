2876a8652e94404388ecf1134eaef8e685fe8073
Spinnafre <61525268+Spinnafre@users.noreply.github.com>
2022-11-02 18:41:41 -0300

    feature : save scores and revert scores not saves

--- app/src/main/java/ufc/smd/esqueleto_placar/PlacarActivity.kt
+++ app/src/main/java/ufc/smd/esqueleto_placar/PlacarActivity.kt
@@ -1,6 +1,7 @@
 package ufc.smd.esqueleto_placar
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -11,26 +12,38 @@ import android.os.Vibrator
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.core.content.getSystemService
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import data.Placar
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
 import java.nio.charset.StandardCharsets
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.ArrayList
 
 class PlacarActivity : AppCompatActivity() {
 
     lateinit var placar: Placar
+    private lateinit var reverter :FloatingActionButton
+    private lateinit var btnSave :FloatingActionButton
+    private val sharedFilename = "PreviousGames"
 
     var game = 0
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_placar)
+        Log.v("Placar Activy Criado","kkkkkkkk")
 
         //Recupera da view anterior os dados do jogo cadastrado
         placar = getIntent().getExtras()?.getSerializable("placar") as Placar
+        btnSave = findViewById(R.id.btSalvarPlacar)
+        reverter = findViewById(R.id.btVerJogos)
 
 
         var tvGolsTime1 = findViewById<TextView>(R.id.GolsTime1)
@@ -47,48 +60,72 @@ class PlacarActivity : AppCompatActivity() {
         //Log.v("PDM", placar.resultado[0].toString());
 
         tvGolsTime1.setOnClickListener{
-            var golsTime1 = placar.resultado.last()[0]
-            var golsTime2 = placar.resultado.last()[1]
+            var golsTime1 = placar.resultados.last()[0]
+            var golsTime2 = placar.resultados.last()[1]
 
             golsTime1++
 
-            var novoPlacar = arrayOf(golsTime1, golsTime2)
+            var novoPlacar = listOf(golsTime1, golsTime2)
 
-            placar.resultado = placar.resultado.plus(novoPlacar)
-
-            modificaPlacar()
+            modificaPlacar(novoPlacar)
+            renderizarPlacar()
         }
 
         tvGolsTime2.setOnClickListener{
-            var golsTime1 = placar.resultado.last()[0]
-            var golsTime2 = placar.resultado.last()[1]
+            var golsTime1 = placar.resultados.last()[0]
+            var golsTime2 = placar.resultados.last()[1]
 
             golsTime2++
 
-            var novoPlacar = arrayOf(golsTime1, golsTime2)
+            var novoPlacar = listOf(golsTime1, golsTime2)
 
-            placar.resultado = placar.resultado.plus(novoPlacar)
-
-            modificaPlacar()
+            modificaPlacar(novoPlacar)
+            renderizarPlacar()
         }
 
+        reverter.setOnClickListener {
+            reverterPlacar()
+        }
+
+        btnSave.setOnClickListener{
+            saveGame()
+            lerUltimosJogos()
+            backToMain()
+
+        }
         //Mudar o nome da partida
 //        val tvNomePartida = findViewById(R.id.tvNomePartida2) as TextView
 //        tvNomePartida.text=placar.nome_partida
 //        ultimoJogos()
     }
 
-    fun modificaPlacar (){
-
-        var tvGolsTime1 = findViewById<TextView>(R.id.GolsTime1)
-        var tvGolsTime2 = findViewById<TextView>(R.id.GolsTime2)
-
-        tvGolsTime1.text = placar.resultado.last()[0].toString()
-        tvGolsTime2.text = placar.resultado.last()[1].toString()
-
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun modificaPlacar (novoPlacar:List<Int>){
+        Log.v("Pronto para inserir >>> ","$novoPlacar")
+        //Limit with offset
+        if(placar.resultados.size >= 10){
+            Log.v("Insert into front >>> ","$novoPlacar")
+            placar.resultados.add(novoPlacar)
+            placar.resultados.removeFirst()
+        }else{
+            placar.resultados.add(novoPlacar)
+        }
+        Log.v("Items","Valores >>> ${placar.resultados}")
+        //Update time
+        var current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")
+        val formatted = current.format(formatter)
+        placar.date = formatted
     }
 
+    fun renderizarPlacar(){
+        var time1 = findViewById<TextView>(R.id.GolsTime1)
+        var time2 = findViewById<TextView>(R.id.GolsTime2)
 
+//        time1.text = placar.resultados.last()[0].toString()
+        time1.text = placar.resultados.last()[0].toString()
+        time2.text = placar.resultados.last()[1].toString()
+    }
     fun vibrar (v:View){
         val buzzer = this.getSystemService<Vibrator>()
          val pattern = longArrayOf(0, 200, 100, 300)
@@ -103,9 +140,17 @@ class PlacarActivity : AppCompatActivity() {
 
     }
 
-    fun saveGame(v: View) {
+    fun reverterPlacar(){
+        var lastScore = placar.resultados.lastOrNull()
+        if(placar.resultados.size > 1){
 
-        val sharedFilename = "PreviousGames"
+            placar.resultados.removeLastOrNull()
+        }
+        Log.v("Revertendo","$lastScore")
+        renderizarPlacar()
+    }
+
+    fun saveGame() {
         val sp: SharedPreferences = getSharedPreferences(sharedFilename, Context.MODE_PRIVATE)
         var edShared = sp.edit()
         //Salvar o número de jogos já armazenados
@@ -122,22 +167,32 @@ class PlacarActivity : AppCompatActivity() {
         edShared.commit() //Não esqueçam de comitar!!!
 
     }
-
-    fun lerUltimosJogos(v: View){
-        val sharedFilename = "PreviousGames"
+    fun backToMain(){
+        val intent = Intent(this, MainActivity::class.java).apply{
+        }
+        startActivity(intent)
+    }
+    fun lerUltimosJogos(){
         val sp: SharedPreferences = getSharedPreferences(sharedFilename, Context.MODE_PRIVATE)
 
-        var meuObjString:String= sp.getString("match1","").toString()
-        if (meuObjString.length >=1) {
-            var dis = ByteArrayInputStream(meuObjString.toByteArray(Charsets.ISO_8859_1))
-            var oos = ObjectInputStream(dis)
-            var placarAntigo:Placar=oos.readObject() as Placar
-            //Log.v("SMD26",placar.resultado[0])
+        var numMatches= sp.getInt("numberMatch",0)
+        if(numMatches === 0){
+            return
         }
+        Log.v("SMD26", numMatches.toString())
+        for(i:Int in  1..numMatches){
+            var meuObjString:String= sp.getString("match"+i,"vazio")!!
+            if (!meuObjString.equals("vazio")) {
+                var dis = ByteArrayInputStream(meuObjString.toByteArray(Charsets.ISO_8859_1))
+                var oos = ObjectInputStream(dis)
+                var placarAntigo:Placar=oos.readObject() as Placar
+                Log.v("SMD26", placarAntigo.resultados.toString())
+            }
+        }
+
     }
 
     fun ultimoJogos () {
-        val sharedFilename = "PreviousGames"
         val sp:SharedPreferences = getSharedPreferences(sharedFilename,Context.MODE_PRIVATE)
         var matchStr:String=sp.getString("match1","").toString()
        // Log.v("PDM22", matchStr)
@@ -145,7 +200,7 @@ class PlacarActivity : AppCompatActivity() {
             var dis = ByteArrayInputStream(matchStr.toByteArray(Charsets.ISO_8859_1))
             var oos = ObjectInputStream(dis)
             var prevPlacar:Placar = oos.readObject() as Placar
-            Log.v("PDM22", "Jogo Salvo:"+ prevPlacar.resultado)
+            Log.v("PDM22", "Jogo Salvo:"+ prevPlacar.resultados)
         }
 
     }
